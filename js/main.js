b8fbaee059a7ed4e689f151334fd58bba333a4b0
Anderson Jean <andersonjean20136@gmail.com>
2021-06-23 16:21:18 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+    function typeWriter(elemento){
+        const textoArray = elemento.innerHTML.split('');
+        elemento.innerHTML ='';
+        textoArray.forEach((letra, i) => {
+            setTimeout(function() {
+                elemento.innerHTML += letra;
+            },75 * i);
+        })
+    }
+
+
+    const titulo = document.querySelector('.chamada');
+    typeWriter(titulo);
+}
